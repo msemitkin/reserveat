3265a827ae95ad5abb54e1613c3894ec990fa5c9
Mykhailo Semitkin <michael.semitkin@gmail.com>
2023-03-09 23:31:24 +0200

    add swagger ui

--- reserveat-server/build.gradle.kts
+++ reserveat-server/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     implementation("org.openapitools:jackson-databind-nullable:0.2.3")
 }
 
---
+++ reserveat-server/src/main/java/com/reserveat/ReserveatApplication.java
@@ -0,0 +1,11 @@
+package com.reserveat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveatApplication.class, args);
+    }
+}
