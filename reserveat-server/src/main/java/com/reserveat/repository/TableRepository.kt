dbf6487ab47f8f9e4377cef4e55cebac48a86f9c
Mykhailo Semitkin <michael.semitkin@gmail.com>
2023-04-09 21:03:14 +0300

    extract row mapper

--- reserveat-server/src/main/java/com/reserveat/repository/TableRepository.kt
+++ reserveat-server/src/main/java/com/reserveat/repository/TableRepository.kt
@@ -4,22 +4,13 @@ import com.reserveat.domain.Table
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Component
+import java.sql.ResultSet
 
 //Component annotation is used here to override default Spring CGLIB proxy used for @Repository's
 @Component
 class TableRepository(
     private val jdbcTemplate: NamedParameterJdbcTemplate
 ) {
-    companion object {
-        var TABLE_ROW_MAPPER = RowMapper { rs, _ ->
-            Table(
-                id = rs.getInt("id"),
-                locationId = rs.getInt("location_id"),
-                numberOfSeats = rs.getInt("number_of_seats")
-            )
-        }
-    }
-
     fun save(table: Table): Table {
         return jdbcTemplate.queryForObject(
             """
@@ -41,10 +32,19 @@ class TableRepository(
                 WHERE location_id = :location_id 
                 """,
             mapOf("location_id" to locationId),
-            TABLE_ROW_MAPPER
+            TableRowMapper
         )
         queryForStream.use {
             return queryForStream.toList()
         }
     }
+}
+
+object TableRowMapper : RowMapper<Table> {
+    override fun mapRow(rs: ResultSet, rowNum: Int): Table =
+        Table(
+            id = rs.getInt("id"),
+            locationId = rs.getInt("location_id"),
+            numberOfSeats = rs.getInt("number_of_seats")
+        )
 }
